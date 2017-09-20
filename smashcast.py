9afb8398cd4105897a8bce6ecfe3d43f33e6d079
JKornberg <Kornberg.jonah@gmail.com>
2017-09-20 10:59:23 -0400

    Working splitting, working split from file, preview doesn't crash

--- smashcast.py
+++ smashcast.py
@@ -10,6 +10,7 @@ kivy.require('1.10.0')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
@@ -22,6 +23,9 @@ from kivy.uix.slider import Slider
 from kivy.uix.image import Image
 from kivy.network.urlrequest import UrlRequest
 from kivy.graphics.texture import Texture
+from kivy.uix.scrollview import ScrollView
+import datetime
+import subprocess as sp
 # With a box layout we arrange widgets in a horizontal
 # or vertical box
 
@@ -40,12 +44,12 @@ class VodCutterLayout(BoxLayout):
 		root.withdraw()
 		dirname = tkinter.filedialog.askopenfile(parent=root, initialdir="/",
                                     title='Please select a directory')
-		self.changeScreen(dirname)
+		self.changeScreen(dirname.name)
 	
 	def changeScreen(self, dirName):
 		print(self.ids)
-		self.manager.get_screen("Split_Vod").displayFirstFrameFromFile(request, 1)
-		self.manager.current = "Split_Vod"							
+		self.parent.manager.get_screen("Split_Vod").displayFirstFrameFromFile(dirName)
+		self.parent.manager.current = "Split_Vod"							
 
 class VodCutterApp(App):
 	def build(self):
@@ -63,27 +67,78 @@ class SplitVod(Screen):
 	INPUT_VOD_PATH = None
 	video = None
 	length = None
+	fps = None
+	lastValue = 0
+	splitCount = 1
+	timeArray = []
+	
+	def time_to_seconds(self, t):
+		hms = t.count(':')
+		if hms == 2:
+			h, m, s = [int(i) for i in t.split(':')]
+			return 3600*h + 60*m + s
+		else:
+			m, s = [int(i) for i in t.split(':')]
+			return 60*m + s
+	
 	def initiateVod(self):
 		self.video = cv2.VideoCapture(self.INPUT_VOD_PATH)
 		self.length = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
-	def displayFirstFrameFromFile(self, dirname):
-		self.INPUT_VOD_PATH = dirname
-		initiateVod()
-		self.displayFrame(self, 0)
+		self.fps= self.video.get(cv2.CAP_PROP_FPS)
+		self.addSplit()
+
+
+	def displayFirstFrameFromFile(self, dirName):
+		print(dirName)
+		print("********")
+		self.INPUT_VOD_PATH = dirName
+		self.initiateVod()
+		self.displayFrame(0, True)
 	
 	def displayFirstFrame(self, request, frame):
 		self.INPUT_VOD_PATH = request.file_path
 		self.initiateVod()
-		self.displayFrame(0)
+		self.displayFrame(0, True)
 		
-	def displayFrame(self, frameNum):
-		frameNum = self.length*frameNum/100
-		self.video.set(1, frameNum)
-		success, frame = self.video.read()
-		frame = np.rot90(np.swapaxes(frame, 0, 1))
-		myTexture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
-		myTexture.blit_buffer(frame.tostring(), colorfmt='bgr', bufferfmt='ubyte')
-		self.ids['FrameDisplay'].texture=myTexture
+	def displayFrame(self, frameNum, override):
+		if frameNum != self.lastValue or override:
+			lastValue = frameNum
+			frameNum = self.length*frameNum/100
+			self.setTime(frameNum)
+			if frameNum == self.length:
+				frameNum -= 1
+			self.video.set(1, frameNum)
+			success, frame = self.video.read()
+			frame = np.rot90(np.swapaxes(frame, 0, 1))
+			myTexture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
+			myTexture.blit_buffer(frame.tostring(), colorfmt='bgr', bufferfmt='ubyte')
+			self.ids['FrameDisplay'].texture=myTexture
+	def addSplit(self):
+		lay = BoxLayout(orientation='horizontal', size_hint=(None,None), size=(200,40), pos_hint={'center_x': .5})
+		start = TextInput(multiline=False,text='00:00')
+		end = TextInput(multiline=False,text='00:00')
+		lay.add_widget(start)
+		lay.add_widget(end)
+		self.ids['output'].add_widget(lay)
+		self.splitCount += 1
+		self.timeArray.append([start,end])
+		print(self.timeArray)
+	
+	
+	def Submit(self):
+		count = 0
+		for time in self.timeArray:
+			startSec = self.time_to_seconds(time[0].text)
+			endSec = self.time_to_seconds(time[1].text)
+			command= ['ffmpeg',  '-ss', str(startSec), '-i', self.INPUT_VOD_PATH, '-t', str(endSec-startSec), '-c', 'copy', 'out' + str(count) + '.flv']
+			count+=1
+			pipe=sp.Popen(command, stdin=sp.PIPE, stderr=sp.PIPE)
+
+	def setTime(self, frameNum):
+		secs = int(frameNum/self.fps)
+		time = str(datetime.timedelta(seconds=secs)) 
+		
+		self.ids['timer'].text= time
 	
 class EnterURL(Screen):
 	def download_file(self, url):
